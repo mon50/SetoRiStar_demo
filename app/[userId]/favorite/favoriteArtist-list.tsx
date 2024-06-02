a9136af381667cc872ceb69a42d435c3e1f60e2c
sothy50 <stea50monte@gmail.com>
2024-06-02 13:52:32 +0900

    feat: #10 UserLiveSchedulePage

--- app/[userId]/favorite/favoriteArtist-list.tsx
+++ app/[userId]/favorite/favoriteArtist-list.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FavoriteArtists } from "@/types/Type";
+import { FavoriteArtists } from "@/types/ArtistType";
 import { createClient } from "@/utils/supabase/client";
 import { Link } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
--- app/[userId]/list/page.tsx
+++ app/[userId]/list/page.tsx
@@ -1,13 +1,12 @@
 // pages/[userId]/page.tsx
-import { createClient } from "@/utils/supabase/server"
 import UserLiveForm from "./userLive-form"
 
-export default async function UserPage() {
-  const supabase = createClient()
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  return <UserLiveForm user={user} />
+export default async function UserPage({
+  params,
+  searchParams,
+}: {
+  params: { userId: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  return <UserLiveForm userId={params.userId} />
 }
